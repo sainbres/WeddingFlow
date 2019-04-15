4cd7244ed6a9f10b5ebdeadee9e71a6ab90d4c91
Gareth <sainbres@gmail.com>
2019-04-15 22:21:37 +0100

    WF-28: Add menu drop-down with logout functionality

--- app/src/main/java/com/sainbres/shu/weddingflow/MainActivity.java
+++ app/src/main/java/com/sainbres/shu/weddingflow/MainActivity.java
@@ -1,14 +1,68 @@
 package com.sainbres.shu.weddingflow;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences SharedPrefs;
+    private SharedPreferences.Editor Editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        Editor = SharedPrefs.edit();
+
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.main_Toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("Home");
+        toolbar.setTitleTextColor(getResources().getColor(R.color.colorWhite));
+        //getSupportActionBar().setIcon(R.drawable.ic_settings);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.settings:
+                intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.help:
+                intent = new Intent(this, HelpActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.logout:
+                Editor.putBoolean(getString(R.string.SP_StayLoggedIn), false);
+                Editor.commit();
+                intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
+
    
