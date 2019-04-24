0224146a939b33ef917927f0103be10b1f1d72df
Gareth <sainbres@gmail.com>
2019-04-24 14:05:29 +0100

    WF-52: Add payments class and table to db

---
+++ app/src/main/java/com/sainbres/shu/weddingflow/Models/Payment.java
@@ -0,0 +1,76 @@
+package com.sainbres.shu.weddingflow.Models;
+
+import com.raizlabs.android.dbflow.annotation.Column;
+import com.raizlabs.android.dbflow.annotation.PrimaryKey;
+import com.raizlabs.android.dbflow.annotation.Table;
+import com.raizlabs.android.dbflow.structure.BaseModel;
+import com.sainbres.shu.weddingflow.WeddingFlowDB;
+
+@Table(database = WeddingFlowDB.class, name = "Payments")
+public class Payment extends BaseModel {
+
+    public Payment() {
+    }
+
+    @PrimaryKey(autoincrement = true)
+    int PaymentId;
+
+    @Column
+    int BudgetId;
+
+    @Column
+    String Name;
+
+    @Column
+    String Memo;
+
+    @Column
+    String Date;
+
+    @Column
+    double Amount;
+
+    @Column
+    String Direction;
+
+    public int getPaymentId() {
+        return PaymentId;
+    }
+    public void setPaymentId(int paymentId) {
+        PaymentId = paymentId;
+    }
+
+    public int getBudgetId() {
+        return BudgetId;
+    }
+    public void getBudgetId(int budgetId) {
+        BudgetId = budgetId;
+    }
+
+    public String getName() { return Name; }
+    public void setName(String name) { Name = name; }
+
+    public String getMemo() { return Memo; }
+    public void setMemo(String memo) { Memo = memo; }
+
+    public String getDate() {
+        return Date;
+    }
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public double getAmount() {
+        return Amount;
+    }
+    public void setAmount(double amount) {
+        Amount = amount;
+    }
+
+    public String getDirection() {
+        return Direction;
+    }
+    public void setDirection(String direction) {
+        Direction = direction;
+    }
+}
